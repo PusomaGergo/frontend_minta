b3ed03bac6c3744afdadc989af9c20e9d5d67447
Pusoma Gergő <pusoma.gergo@jedlik.helyi>
2022-03-29 11:20:05 +0200

    save2

--- hahu-app/src/App.vue
+++ hahu-app/src/App.vue
@@ -1,98 +1,14 @@
 <template>
-  <div class="container">
-    <!-- A grey horizontal navbar that becomes vertical on small screens -->
-    <nav class="navbar navbar-expand-sm bg-light">
-      <div class="container-fluid">
-        <!-- Links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link 1</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link 2</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link 3</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <!-- Carousel -->
-
-    <div id="demo" class="carousel slide" data-bs-ride="carousel">
-      <!-- Indicators/dots -->
-      <div class="carousel-indicators">
-        <button
-          type="button"
-          data-bs-target="#demo"
-          data-bs-slide-to="0"
-          class="active"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#demo"
-          data-bs-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          data-bs-target="#demo"
-          data-bs-slide-to="2"
-        ></button>
-      </div>
-
-      <!-- The slideshow/carousel -->
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img
-            src="https://picsum.photos/id/237/100/150"
-            alt="Los Angeles"
-            class="d-block w-100"
-          />
-        </div>
-        <div class="carousel-item">
-          <img
-            src="https://picsum.photos/seed/picsum/200/300"
-            alt="Chicago"
-            class="d-block w-100"
-          />
-        </div>
-        <div class="carousel-item">
-          <img
-            src="https://picsum.photos/200/300?grayscale"
-            alt="New York"
-            class="d-block w-100"
-          />
-        </div>
-      </div>
-
-      <!-- Left and right controls/icons -->
-      <button
-        class="carousel-control-prev"
-        type="button"
-        data-bs-target="#demo"
-        data-bs-slide="prev"
-      >
-        <span class="carousel-control-prev-icon"></span>
-      </button>
-      <button
-        class="carousel-control-next"
-        type="button"
-        data-bs-target="#demo"
-        data-bs-slide="next"
-      >
-        <span class="carousel-control-next-icon"></span>
-      </button>
-    </div>
-  </div>
+  <open-page></open-page>
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
+import OpenPage from "./components/OpenPage.vue";
 
 export default {
   name: "App",
   components: {
-    // HelloWorld,
+    OpenPage,
   },
 };
 </script>
--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+import "bootstrap/dist/js/bootstrap";
+app.mount("#app");
