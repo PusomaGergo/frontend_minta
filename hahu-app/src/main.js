3722451876e3d2030578c8f3ec362f3174848b26
Pusoma Gergő <pusoma.gergo@jedlik.helyi>
2022-03-29 11:33:43 +0200

    03.29.-router elokeszitese

--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -1,9 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { createRouter, createwebHistory } from "vue-router";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-const app = createApp(App);
+const router = createRouter({
+  history: createwebHistory(),
+  routes: [
+    { path: "/", component: OpenPage },
+    { path: "/offers", component: Ingatlankinalat },
+  ],
+});
 
+const app = createApp(App);
+app.use(router);
 import "bootstrap/dist/js/bootstrap";
 app.mount("#app");
