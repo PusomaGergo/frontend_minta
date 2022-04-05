2c71f24c179132a59bd983a08b582d36d8e5cc91
Pusoma Gergő <pusomagergo12@gmail.com>
2022-04-05 10:59:44 +0200

    mentes

--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { createRouter, createwebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import "bootstrap/dist/css/bootstrap.css";
 import OpenPage from './components/OpenPage.vue';
 import IngatlanKinalat from './components/IngatlanKinalat.vue';
+import UjHirdetes from './components/UjHirdetes.vue';
 
 import VueGoodTablePlugin from 'vue-good-table-next';
 
@@ -13,17 +14,18 @@ import 'vue-good-table-next/dist/vue-good-table-next.css'
 
 
 
-
 const router = createRouter({
-  history: createwebHistory(),
-  routes: [
-    { path: "/", component: OpenPage },
-    { path: "/offers", component: IngatlanKinalat },
-  ],
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: OpenPage },
+        { path: "/offers", component: IngatlanKinalat },
+        { path: "/newad", component: UjHirdetes },
+    ],
 });
 
 const app = createApp(App);
+
 app.use(VueGoodTablePlugin);
 app.use(router);
-import "bootstrap/dist/js/bootstrap";
+
 app.mount("#app");
